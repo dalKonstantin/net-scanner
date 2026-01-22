bc654119a678bc107622fdafab67e1f2db2ed413
dalKonstantin <konser.dal@yandex.com>
2026-01-23 01:17:56 +0300

    Improved output 2
    One more progress bar
    Output is now sorted by ip

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+use indicatif::ProgressBar;
 use std::collections::HashMap;
-use std::process::ExitCode;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
-use indicatif::ProgressBar;
 
+use clap::Parser;
 use pnet::datalink;
 use pnet::datalink::Channel::Ethernet;
-use pnet::packet::Packet;
 use pnet::packet::arp::{ArpOperations, ArpPacket};
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::Packet;
 use std::error::Error;
 
 use crate::utils::{
@@ -18,11 +18,20 @@ use crate::utils::{
 
 mod utils;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    iface: String,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let ipmac_table = Arc::new(Mutex::new(HashMap::new()));
     let table_for_thread = ipmac_table.clone();
     // get interface name from commad line args
-    let iface_name = std::env::args().nth(1).ok_or("Specify interface name")?;
+    let args = Args::parse();
+    let iface_name = args.iface;
+    //let iface_name = std::env::args().nth(1).ok_or("Specify interface name")?;
 
     let iface =
         get_interface_by_name(&iface_name).ok_or("Cannot find interface with specified name")?;
@@ -41,45 +50,54 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     // RECEIVE IN THREAD
-    let rcv_thread = std::thread::spawn(move || {
-        loop {
-            match rx.next() {
-                Ok(packet) => {
-                    let eth_packet = EthernetPacket::new(packet).unwrap();
+    let _rcv_thread = std::thread::spawn(move || loop {
+        match rx.next() {
+            Ok(packet) => {
+                let eth_packet = EthernetPacket::new(packet).unwrap();
 
-                    if eth_packet.get_ethertype() == EtherTypes::Arp {
-                        if let Some(arp_packet) = ArpPacket::new(eth_packet.payload()) {
-                            if arp_packet.get_operation() == ArpOperations::Reply
-                                && arp_packet.get_target_hw_addr() == local_mac
-                            {
-                                let sender_ip = arp_packet.get_sender_proto_addr();
-                                let sender_mac = arp_packet.get_sender_hw_addr();
+                if eth_packet.get_ethertype() == EtherTypes::Arp {
+                    if let Some(arp_packet) = ArpPacket::new(eth_packet.payload()) {
+                        if arp_packet.get_operation() == ArpOperations::Reply
+                            && arp_packet.get_target_hw_addr() == local_mac
+                        {
+                            let sender_ip = arp_packet.get_sender_proto_addr();
+                            let sender_mac = arp_packet.get_sender_hw_addr();
 
-                                let mut table = table_for_thread.lock().unwrap();
-                                table.insert(sender_ip, sender_mac);
-                            }
+                            let mut table = table_for_thread.lock().unwrap();
+                            table.insert(sender_ip, sender_mac);
                         }
                     }
                 }
-                Err(e) => panic!("Error: {}", e),
             }
+            Err(e) => panic!("Error: {}", e),
         }
     });
     // send arp packet to all hosts in ips range;
     println!("Sending arp discovery...");
-    let bar = ProgressBar::new(usable_ips.len() as u64);
-    for ip in usable_ips {
-        bar.inc(1);
-        send_arp_discovery(&mut *tx, ip, local_ip, local_mac);
+    let disc_bar = ProgressBar::new(usable_ips.len() as u64);
+    for ip in &usable_ips {
+        send_arp_discovery(&mut *tx, *ip, local_ip, local_mac);
+        disc_bar.inc(1);
         std::thread::sleep(Duration::from_millis(10));
     }
-    std::thread::sleep(Duration::from_secs(10));
-    // rcv_thread.join().unwrap();
+    disc_bar.finish();
+
+    println!("Waiting for responses... ");
+    let wait_time = (usable_ips.len() * 10) as u64 + 50;
+
+    let wait_bar = ProgressBar::new(wait_time);
+    for _ in 0..wait_time {
+        wait_bar.inc(1);
+        std::thread::sleep(Duration::from_millis(10));
+    }
+    wait_bar.finish();
 
     let final_table = ipmac_table.lock().unwrap();
 
-    println!("Found hosts:");
-    for (ip, mac) in final_table.iter() {
+    let mut sorted_table: Vec<_> = final_table.iter().collect();
+    sorted_table.sort_by_key(|(ip, _)| *ip);
+    println!("Found {} hosts:", sorted_table.len());
+    for (ip, mac) in sorted_table.iter() {
         println!("IP:{:<15} MAC: {}", ip, mac);
     }
 
